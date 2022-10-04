914de6267368cfdf60cd6f8fa88146e4042c127b
Hasan Faraz Khan <farazkhan138@gmail.com>
2022-10-05 00:11:41 +0530

    Fetch API (#5)
    * usefetch
    
    * Event API
    
    * fetchDate, parseDate merged
    
    Co-authored-by: Hamdan Zaheer <shade0987insane@gmail.com>

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,28 +1,31 @@
+import {fetchDate, convertDate} from '../../utils/parseDate';
+// import fetchDate from '../../utils/fetchDate';
 import "./News.css";
 
-const News = () => {
+const News = (props) => {
+  const {data} = props;
+  const {day_num, day} = fetchDate(data.start_date);
   return (
     <div className="event-component">
       <h1 className="upcoming-event">Upcoming Event</h1>
       <div className="event-component-header">
         <h1 className="event-title">
-          Workshop "Getting Started with Web Development" to get you started
-          with web developemnt delevered by best instructor of AMU, Hasan Faraz
+          {data.title}
         </h1>
         <h1 className="start-event">
-          13 <span>mon</span>
+          {day_num} <span>{day}</span>
         </h1>
       </div>
       <div className="event-details">
         <p className="event-detail-header">Details</p>
         <p className="details">
-          Start :<span className="detail-value">13:07 - 01 - 02- 2022</span>
+          Start :<span className="detail-value">{convertDate(data.start_date)}</span>
         </p>
         <p className="details">
-          End :<span className="detail-value">12:00 - 15 - 10- 2022</span>
+          End :<span className="detail-value">{convertDate(data.end_date)}</span>
         </p>
         <p className="details">
-          Venue :<span className="detail-value">13:07 - 01 - 02- 2022</span>
+          Venue :<span className="detail-value">{data.venue}</span>
         </p>
       </div>
     </div>
