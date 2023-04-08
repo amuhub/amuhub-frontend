6b692ca65983b169104a08657441f5c0428a2c4d
farazkhanfk7 <farazkhan138@gmail.com>
2023-04-08 16:58:44 +0530

    photos in profile

--- src/components/ProfilePosts/ProfilePost.js
+++ src/components/ProfilePosts/ProfilePost.js
@@ -2,16 +2,17 @@ import React from "react";
 import "./ProfilePosts.css";
 import postImg from "../Post/img.png";
 
-const ProfilePost = () => {
+const ProfilePost = (props) => {
+  const { postItem } = props;
   return (
     <div className="profile-post-container">
-      <img src={postImg} alt="post-profile"></img>
+      <img src={postItem.photo} alt="post-profile"></img>
       <div className="profile-post-overlay">
         <span className="post-likes">
-          20<i className="fas fa-heart"></i>
+          {postItem.likes.length}<i className="fas fa-heart"></i>
         </span>
         <span className="post-comments">
-          14<i className="fas fa-comment"></i>
+        {postItem.comments.length}<i className="fas fa-comment"></i>
         </span>
       </div>
     </div>
--- src/components/ProfilePosts/ProfilePosts.js
+++ src/components/ProfilePosts/ProfilePosts.js
@@ -4,7 +4,9 @@ import { useParams } from "react-router-dom";
 import postImg from "../Post/img.png";
 import { useFetch } from "../../utils/useFetch";
 import baseUrl from "../../utils/constants";
-import ProfilePost from "./ProfilePost"
+import NoContent from "../NoContent/NoContent";
+import { InfinitySpin } from "react-loader-spinner";
+import ProfilePost from "./ProfilePost";
 
 const ProfilePosts = () => {
   const { username } = useParams();
@@ -14,52 +16,21 @@ const ProfilePosts = () => {
   console.log("profile posts", data);
 
   return (
-    <div className="post-grid">
-      <div className="profile-post-container">
-        <img src={postImg} alt="post-profile"></img>
-        <div className="profile-post-overlay">
-          <span className="post-likes">
-            20<i className="fas fa-heart"></i>
-          </span>
-          <span className="post-comments">
-            14<i className="fas fa-comment"></i>
-          </span>
-        </div>
-      </div>
-      <div className="profile-post-container">
-        <img src={postImg} alt="post-profile"></img>
-        <div className="profile-post-overlay">
-          <span className="post-likes">
-            20<i className="fas fa-heart"></i>
-          </span>
-          <span className="post-comments">
-            14<i className="fas fa-comment"></i>
-          </span>
-        </div>
-      </div>
-
-      <div className="profile-post-container">
-        <img src={postImg} alt="post-profile"></img>
-        <div className="profile-post-overlay">
-          <span className="post-likes">
-            20<i className="fas fa-heart"></i>
-          </span>
-          <span className="post-comments">
-            14<i className="fas fa-comment"></i>
-          </span>
-        </div>
-      </div>
-
-      <div className="profile-post-container">
-        <img src={postImg} alt="post-profile"></img>
-        <div className="profile-post-overlay">
-          <span className="post-likes">
-            20<i className="fas fa-heart"></i>
-          </span>
-          <span className="post-comments">
-            14<i className="fas fa-comment"></i>
-          </span>
-        </div>
+    <div>
+      {pending && (
+        <InfinitySpin type="ThreeDots" color="#00BFFF" height={80} width={80} />
+      )}
+      {error && <div>{error}</div>}
+      {!pending && !data.data.length && 
+      (
+        <NoContent text={"You haven't asked any questions yet!"}/>
+      )}
+      <div className="post-grid">
+      {!pending &&
+        data &&
+        data.data.map((postItem) => (
+          <ProfilePost postItem={postItem} key={postItem._id} />
+        ))}
       </div>
     </div>
   );
