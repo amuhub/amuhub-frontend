c49d70d636397584ee74ba97e15da8f727cd179d
Hasan Faraz Khan <farazkhan138@gmail.com>
2023-04-30 21:16:47 +0530

    follow/follower & question (#24)

--- src/components/SocialOverlay/SocialOverlay.js
+++ src/components/SocialOverlay/SocialOverlay.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./SocialOverlay.css";
-import ProfileOverview from "../ProfileOverview/ProfileOverview";
+import ProfileCard from "./ProfileCard";
+import { useFetchToken } from "../../utils/useFetch";
+import NoContent from "../NoContent/NoContent";
+import { InfinitySpin } from "react-loader-spinner";
+import baseUrl from "../../utils/constants";
 
-const SocialOverlay = ({
-  setSocialToggler,
-  overlayID,
-  followers,
-  following,
-}) => {
-  const data = overlayID === "following" ? following : followers;
+const SocialOverlay = ({ setSocialToggler, overlayID, username }) => {
+  const token = localStorage.getItem("token");
+
+  const { data, pending, error } = useFetchToken(
+    `${baseUrl}/profile/${username}/social/?social=${overlayID}`,
+    token
+  );
 
   return (
     <div className="social-overlay">
@@ -17,10 +21,22 @@ const SocialOverlay = ({
       </button>
       <div className="social-container">
         <p className="social-title">{overlayID}</p>
-        {data.map((index, elem) => (
-          <ProfileOverview key={index} />
-        ))}
-        <ProfileOverview />
+        {/* {pending && (
+          <InfinitySpin type="ThreeDots" color="#00BFFF" height={80} width={80} />
+        )}
+        {error && <div>{error}</div>}
+        {!pending && !data.data.length && (
+          <NoContent text={`You don't have any ${overlayID} yet !`} />
+        )} */}
+        {!pending &&
+          data &&
+          data.data.map((item) => (
+            <ProfileCard
+              data={item}
+              setSocialToggler={setSocialToggler}
+              key={item._id}
+            />
+          ))}
       </div>
     </div>
   );
